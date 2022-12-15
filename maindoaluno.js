2b9fb0292126bcb430119acc0fe581da72297ec7
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2022-12-15 10:23:13 -0300

    Update maindoaluno.js

--- maindoaluno.js
+++ maindoaluno.js
@@ -36,6 +36,55 @@ recognition.??? = function(event) {
 //Programe 'Se conteúdo for igual a 'tire minha selfie', aconteça a função 'speak'
 
      ????
+};
+
+function speak(){
+    var synth = window.speechSynthesis;
+    //A voz da API irá falar 'Tirando sua selfie em 5 segundos', 
+    //Utilize o código 'speak_data' para isso
+    ??? = "Tirando sua selfie em 5 segundos";
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    //Anexe a variável 'Webcam' na camera
+    ????.attach(camera);
+
+    
+    //Declare a função 'setTimeOut, pois ela define o tempo em que a imagem será tirada e salva
+    ???(function()
+    { 
+        take_selfie(); 
+        //Coloque o nome da função que salva a imagem
+        ???();
+        //Defina em quantos milisegundos a foto será tirada
+    }, 5000);
+}
+
+ 
+camera = document.getElementById("camera");
+Webcam.set({
+    //Defina a largura e altura da camera
+    ???:360,
+    ???:250,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_selfie()
+{
+    Webcam.snap(function(data_uri) {
+        //Utilize o código que altera o HTML, pois você quer ver a imagem na tela
+        document.getElementById("result").??? = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
 }
 
 
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
